23211ace2dfb7d277bb6a1524be7e468b8785eb5
Ricardo <ricardo.baldrez@gmail.com>
2020-09-02 01:46:20 -0300

    Armazenando no estado, listando todos os produtos vindos da API

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import api from "../../services/api";
 
 export default class Main extends Component {
+    // Criando o armazenamento em estado
+    state = {
+        products: [],
+    };
+
     // Método que é executado assim que o componente é mostrado em tela
     // Então se precisar executar uma ação logo que o componente é exibido em tela utilizar 'componentDidMount()'
     componentDidMount() {
@@ -11,10 +16,21 @@ export default class Main extends Component {
 
     loadProducts = async () => {
         const response = await api.get("/products");
-        console.log(response.data.docs);
+        
+        // Setando o retorno no estado definido à cima
+        this.setState({
+            products: response.data.docs
+        });
     };
 
     render() {
-        return <h1>Hello RockeSeat</h1>
+        return (
+            <div className="product-list">
+                {this.state.products.map(product => (
+                    // Sempre que faz um map, o react pede para que no primeiro elemento seja adicionado uma key com o valor único para cada item da iteração
+                    <h2 key={product._id}>{ product.title }</h2>
+                ))};
+            </div>
+        )
     }
 }
