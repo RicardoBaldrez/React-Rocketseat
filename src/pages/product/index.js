80d3fde36e94e538a905c8d5e34a1bf44b279f0b
Ricardo <ricardo.baldrez@gmail.com>
2020-09-02 16:35:31 -0300

    Navegando pro detalhe do produto clicado

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-function Product() {
-    return (
-        <h1>Details Product</h1>
-    )
+import api from '../../services/api';
+
+import './styles.css';
+
+export default class Product extends Component {
+    state = {
+        product: {},
+    }
+
+    async componentDidMount() {
+        const { id } = this.props.match.params;
+
+        const response = await api.get(`/products/${id}`);
+        console.log(response);
+
+        this.setState({
+            product: response.data
+        });
+    }
+
+    render() {
+        const { product } = this.state;
+
+        return (
+            <div className="product-info">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+
+                <p>
+                    URL: <a href={product.url}>{product.url}</a>
+                </p>
+
+                <div className="contLinkBackHome">
+                    <Link to="/" className="linkBackHome">
+                        Voltar a página principal
+                    </Link>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default Product;
